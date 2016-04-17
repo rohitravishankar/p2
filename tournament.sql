006cb781646e29ffccb0e473bd549e4911213601
Rohit Ravishankar <rohitravishankar@users.noreply.github.com>
2016-04-17 23:04:19 +0530

    Update tournament.sql

--- tournament.sql
+++ tournament.sql
@@ -4,10 +4,13 @@
 -- create db tournament
 -- psql tournament
 
+
+--Table players contains a unique id for each player corresponding to his/her name and time of creation
 CREATE TABLE players  ( id serial primary key,
                         name varchar (25) not null,
                         created_at timestamp);
 
+--Table matches contains all match related stats
 CREATE TABLE matches  ( id serial primary key,
                         winner_id int,
                         loser_id int,
